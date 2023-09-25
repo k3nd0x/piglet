5fb574bc21425ec458743ba3085fe4d7166edd19
k3nd0x <dev0@it-zs.de>
2023-09-25 10:24:14 +0200

    Piglet Bugfix - Show all notifications doesn't work

--- webapp/app/views/notifications.py
+++ webapp/app/views/notifications.py
@@ -35,7 +35,7 @@ def notifications():
         pigapi = api(auth=session["authorization"])
         noticount, notilist, notifications = get_notis(pigapi)
 
-        s, notis = pigapi.get(f"notifications/?show_all=false")
+        s, notis = pigapi.get(f"notifications/?show_all=true")
         all_noticount = notis[0]
         all_notilist = notis[1]
         all_notifications = notis[2]
